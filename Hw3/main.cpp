8f8eddb909b66bcc40fed1913b825e428ccff2de
skywoodsz <skywoodsz@stu.edu.cn>
2021-12-22 21:16:41 +0800

    update reproject

--- Hw3/main.cpp
+++ Hw3/main.cpp
@@ -59,7 +59,7 @@ int main()
         cv::drawChessboardCorners(ImageGray, board_size,
           image_points_buf, true); // 绘制
         cv::imshow("ImageGrayCorner", ImageGray);
-        cv::waitKey(100); // 0.5s
+        // cv::waitKey(100); // 0.5s
     }
 
     // step3: 創建世界坐標系
@@ -82,7 +82,7 @@ int main()
         }
         object_points.push_back(tempPoint);
     }
-    
+
     // step4: 標定
     std::cout<<"strat to calibrate"<< std::endl;
 
@@ -108,5 +108,46 @@ int main()
     fout<<cameraMatrix<<endl<<endl;
     fout<<"Camera distCoeffs：\n";
     fout<<distCoeffs<<endl<<endl<<endl;
+
+    // reprojection
+    for (int i=0;i<image_count;i++)
+    {
+        point_counts.push_back(board_size.width*board_size.height);
+    }
+
+    double total_err = 0.0;
+    double err = 0.0;
+    vector<Point2f> image_points2;
+    cout<<"\t每幅图像的标定误差：\n";
+    for (int i=0;i<image_count;i++)
+    {
+        vector<Point3f> tempPointSet=object_points[i];
+
+        projectPoints(tempPointSet,rvecsMat[i],
+                      tvecsMat[i],cameraMatrix,
+                      distCoeffs,image_points2);
+        cout<<"reprojection:"<<endl;
+
+        vector<Point2f> tempImagePoint = image_points_seq[i];
+        Mat tempImagePointMat = Mat(1,tempImagePoint.size(),CV_32FC2);
+        Mat image_points2Mat = Mat(1,image_points2.size(), CV_32FC2);
+        cout<<"tempImagePoint.size():"<<tempImagePoint.size()<<endl;
+        for (int j = 0 ; j < tempImagePoint.size(); j++)
+        {
+            image_points2Mat.at<Vec2f>(0,j) = Vec2f(image_points2[j].x, image_points2[j].y);
+            tempImagePointMat.at<Vec2f>(0,j) = Vec2f(tempImagePoint[j].x, tempImagePoint[j].y);
+        }
+
+        err = norm(image_points2Mat, tempImagePointMat, NORM_L2);
+        cout<<"err"<<err<<endl;
+        total_err += err/=  point_counts[i];
+        cout<<"total_err"<<total_err<<endl;
+        std::cout<<"第"<<i+1<<"幅图像的平均误差："<<err<<"像素"<<endl;
+
+    }
+    std::cout<<"总体平均误差："<<total_err/image_count<<"像素"<<endl;
+    std::cout<<"评价完成！"<<endl;
+
     return 0;
 };
+
